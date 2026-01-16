5c2ed22ce8e85a81a0fa868ec0a4f3195c8b56ca
gywns7507 <90439116+gywns7507@users.noreply.github.com>
2026-01-16 09:37:03 +0900

    [프로그래머스] L0 - 몫 구하기 (#7)

--- programmers/L0_기초입문/입문/007_몫_구하기.py
+++ programmers/L0_기초입문/입문/007_몫_구하기.py
@@ -3,7 +3,7 @@
 # 문제 링크: https://school.programmers.co.kr/learn/courses/30/lessons/120805
 # 알고리즘: 기초
 # 작성자: 김효준
-# 작성일: 2026. 01. 16. 09:36:46
+# 작성일: 2026. 01. 16. 09:37:02
 
 def solution(num1, num2):
     answer = num1 // num2
